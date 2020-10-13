42c7e39179694bc018efafc5f2a70873b8d2f798
Jordan Wilking <jordan.wilking@connexta.com>
2020-10-13 10:38:06 -0700

    Add datetimepicker that uses mui TextField

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/fields/date.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/fields/date.tsx
@@ -14,12 +14,9 @@
  **/
 import * as React from 'react'
 import { DateInput, IDateInputProps } from '@blueprintjs/datetime'
-
-// @ts-ignore ts-migrate(7016) FIXME: Could not find a declaration file for module '../s... Remove this comment to see the full error message
-import user from '../singletons/user-instance'
 import { DateHelpers } from './date-helpers'
 import { MuiOutlinedInputBorderClasses } from '../theme/theme'
-import { useBackbone } from '../selection-checkbox/useBackbone.hook'
+import useTimePrefs from './useTimePrefs'
 
 type DateFieldProps = {
   value: string
@@ -31,25 +28,15 @@ type DateFieldProps = {
 }
 
 const validateShape = ({ value, onChange }: DateFieldProps) => {
-  if (DateHelpers.Blueprint.commonProps.parseDate(value) === null) {
+  if (!value || DateHelpers.Blueprint.commonProps.parseDate(value) === null) {
     onChange(new Date().toISOString())
   }
 }
 
 export const DateField = ({ value, onChange, BPDateProps }: DateFieldProps) => {
-  const { listenTo } = useBackbone()
-  const [forceRender, setForceRender] = React.useState(Math.random())
+  useTimePrefs()
   React.useEffect(() => {
     validateShape({ onChange, value })
-  }, [])
-  React.useEffect(() => {
-    listenTo(
-      user.getPreferences(),
-      'change:dateTimeFormat change:timeZone',
-      () => {
-        setForceRender(Math.random())
-      }
-    )
   }, [])
 
   return (
